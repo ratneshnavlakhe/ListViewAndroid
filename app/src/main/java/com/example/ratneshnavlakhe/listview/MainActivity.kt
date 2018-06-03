726e685bae97539f2be6563cc46c3114e2918f0d
Ratnesh <ratnesh.navlakhe@go-jek.com>
2018-06-03 13:15:38 +0530

    Add a list main row layout for render rows.

--- app/src/main/java/com/example/ratneshnavlakhe/listview/MainActivity.kt
+++ app/src/main/java/com/example/ratneshnavlakhe/listview/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
@@ -17,8 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listView = findViewById<ListView>(R.id.main_listview)
-        val redColor = Color.parseColor("#FF0000")
-//        listView.setBackgroundColor(redColor)
 
         listView.adapter = MyCustomAdapter(this)
     }
@@ -27,15 +26,26 @@ class MainActivity : AppCompatActivity() {
 
         private val mContext: Context
 
+        private val names = arrayListOf<String>(
+                "Ratnesh", "Sonali", "Kishore", "Sulochana"
+        )
+
         init {
             mContext = context
         }
 
         // responsible for rendering out each row
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val textView = TextView(mContext)
-            textView.text = "Here is my row for my listview"
-            return textView
+            val layoutInflater = LayoutInflater.from(mContext)
+            val rowMain = layoutInflater.inflate(R.layout.main_row, parent, false)
+
+            val nameTextView = rowMain.findViewById<TextView>(R.id.name_textview)
+            nameTextView.text = names.get(position)
+
+            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+            positionTextView.text = "Row number: $position"
+
+            return rowMain
         }
 
         //
@@ -49,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 
         //responsible for how many rows in my list
         override fun getCount(): Int {
-            return 5
+            return names.size
         }
     }
 }
